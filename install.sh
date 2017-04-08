76f20b17a79482085dd9343a8660cd01720f0cd1
jerryxhe <rebound1618@gmail.com>
2017-04-08 03:49:36 +0000

    comments

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y python3 python3-pip
 sudo pip3 install sortedcontainers
 sudo pip install sortedcontainers
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt # this is somewhat unnecessary, but in case the user is using homebrew python or anaconda
 pip install -r requirements.txt
